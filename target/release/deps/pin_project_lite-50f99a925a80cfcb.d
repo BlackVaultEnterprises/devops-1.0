421b9e5415294c57c9fb5f83c62e458349c4af2a
Black Vault Enterprises <blackvaultenterprises@gmail.com>
2025-07-28 23:52:59 -0400

    Add new fingerprint files for libm, pin-project-lite, and version-check

---
+++ target/release/deps/pin_project_lite-50f99a925a80cfcb.d
@@ -0,0 +1,7 @@
+C:\Users\Johnny\Desktop\DevOps 1.0\target\release\deps\pin_project_lite-50f99a925a80cfcb.d: C:\Users\Johnny\scoop\persist\rustup\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pin-project-lite-0.2.16\src\lib.rs
+
+C:\Users\Johnny\Desktop\DevOps 1.0\target\release\deps\libpin_project_lite-50f99a925a80cfcb.rlib: C:\Users\Johnny\scoop\persist\rustup\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pin-project-lite-0.2.16\src\lib.rs
+
+C:\Users\Johnny\Desktop\DevOps 1.0\target\release\deps\libpin_project_lite-50f99a925a80cfcb.rmeta: C:\Users\Johnny\scoop\persist\rustup\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pin-project-lite-0.2.16\src\lib.rs
+
+C:\Users\Johnny\scoop\persist\rustup\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pin-project-lite-0.2.16\src\lib.rs:
